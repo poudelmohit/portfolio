9c0e313a6d4588427b9a6764980f9de088a34c66
Mohit Poudel <53567223+mohit254@users.noreply.github.com>
2023-01-02 00:19:09 +0545

    Update cv.py

--- cv.py
+++ cv.py
@@ -74,7 +74,7 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write('\n')
-st.subheader("Experience & Qulifications")
+st.subheader("Experience & Qualifications")
 st.write(
     """
 - ✔️ Commandable knowlegde on Bash Scriptiong, Git, R MarkDown
